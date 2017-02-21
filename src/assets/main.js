e47fd4444cb947b2f422ee595028b34de08b4927
AlexL70 <alexander.levinson.70@gmail.com>
2017-02-21 22:07:37 +0200

    add other badges' properties

--- src/assets/main.js
+++ src/assets/main.js
@@ -8,8 +8,15 @@ $(function() {
                 var courseDiv = $('<div></div>');
                 courseDiv.addClass('course');
                 courseDiv.append($('<h3></h3>').html(course.title));
+                courseDiv.append($('<img>').attr('src', course.badge));
+                var anchor = $('<a>');
+                anchor.attr('href', course.url);
+                anchor.attr('target', '_blank');
+                anchor.addClass('btn');
+                anchor.addClass('btn-primary');
+                anchor.html('See Course');
+                courseDiv.append(anchor);
                 badges.append(courseDiv);
-                console.log(course.title);
             });
         }
     });
