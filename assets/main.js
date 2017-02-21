7c9123afaa8a2d20214e4e6ab41ae942a91b1099
AlexL70 <alexander.levinson.70@gmail.com>
2017-02-21 21:58:01 +0200

    Show course titles on page

--- assets/main.js
+++ assets/main.js
@@ -3,7 +3,12 @@ $(function() {
         url: 'https://www.codeschool.com/users/2712583.json',
         datatype: 'jsonp',
         success: function(response) {
+            var badges = $('#badges');
             $.each(response.courses.completed, function(index, course) {
+                var courseDiv = $('<div></div>');
+                courseDiv.addClass('course');
+                courseDiv.append($('<h3></h3>').html(course.title));
+                badges.append(courseDiv);
                 console.log(course.title);
             });
         }
