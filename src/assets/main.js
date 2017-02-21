7f6afbe9db6a813bc71c5585cef7a9405de55eaa
AlexL70 <alexander.levinson.70@gmail.com>
2017-02-21 21:46:27 +0200

    get finished courses from Code School site

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,11 @@
 $(function() {
-
-  // your code will go here
-
+    $.ajax({
+        url: 'https://www.codeschool.com/users/2712583.json',
+        datatype: 'jsonp',
+        success: function(response) {
+            $.each(response.courses.completed, function(index, course) {
+                console.log(course.title);
+            });
+        }
+    });
 });
